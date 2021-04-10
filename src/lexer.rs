e244be7082b5c7bfc77f81656e9b1f146bfdeda5
Paul Biberstein <biberstein.paul@gmail.com>
2021-04-09 23:59:36 -0400

    Add initial lexer and main

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,8 +4,12 @@ fn string_token(lex: &mut Lexer<Token>) -> Option<String> {
     let slice = lex.slice();
     Some(slice[1..slice.len() - 1].into())
 }
-#[derive(Logos)]
+#[derive(Logos, Debug, PartialEq)]
 pub enum Token {
+    #[error]
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Error,
+
     #[token("{")]
     LBracket,
     #[token("}")]
@@ -20,15 +24,47 @@ pub enum Token {
     If,
     #[token("-")]
     Minus,
-    #[token("[0-9]+", |lex| lex.slice().parse())]
+    #[token("+")]
+    Plus,
+    #[token("*")]
+    Times,
+    #[regex("[0-9]+", |lex| lex.slice().parse())]
     Number(i64),
-
     #[token("[a-zA-Z][a-zA-z0-9]*", |lex| lex.slice().parse())]
     Identifier(String),
 
     #[token(r#""[^"]*""#, string_token)]
     String(String),
-    #[error]
-    #[regex(r"[ \t\n\f]+", logos::skip)]
-    Error,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lexes_number() {
+        let mut lex = Token::lexer("1");
+
+        assert_eq!(lex.next(), Some(Token::Number(1)));
+    }
+
+    #[test]
+    fn lexes_numbers_and_ops() {
+        let mut lex = Token::lexer("1 + 2 * 3");
+
+        assert_eq!(lex.next(), Some(Token::Number(1)));
+        assert_eq!(lex.slice(), "1");
+
+        assert_eq!(lex.next(), Some(Token::Plus));
+        assert_eq!(lex.slice(), "+");
+
+        assert_eq!(lex.next(), Some(Token::Number(2)));
+        assert_eq!(lex.slice(), "2");
+
+        assert_eq!(lex.next(), Some(Token::Times));
+        assert_eq!(lex.slice(), "*");
+
+        assert_eq!(lex.next(), Some(Token::Number(3)));
+        assert_eq!(lex.slice(), "3");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+mod ast;
 mod lexer;
+mod parse;
+
 use logos::Logos;
 fn main() {
     let program = "1 + 2";
 
-    let mut lexer = lexer::Token::lexer(program);
+    let lexer = lexer::Token::lexer(program);
+    let _ = lexer;
 
-    lexer.next();
+    let input = &mut vec![
+        lexer::Token::Number(3),
+        lexer::Token::Times,
+        lexer::Token::Number(2),
+        lexer::Token::Plus,
+        lexer::Token::Number(1),
+    ];
+    input.reverse();
+    let result = parse::parse_expr(input, None).unwrap();
+    let _ = result;
 }
