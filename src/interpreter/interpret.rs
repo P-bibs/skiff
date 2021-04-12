07fc020027ec22d409a0733e3453275477952329
Paul Biberstein <biberstein.paul@gmail.com>
2021-04-12 13:28:20 -0400

    Refactor interpreter

--- src/interpreter/interpret.rs
+++ src/interpreter/interpret.rs
@@ -12,23 +12,17 @@ fn interpret_env(expr: Ast, env: HashMap<String, Val>) -> Val {
         Ast::OperatorNode(Op::Plus, e1, e2) => {
             let v1 = interpret_env(*e1, env.clone());
             let v2 = interpret_env(*e2, env.clone());
-            match v1 {
-                Val::Num(n1) => match v2 {
-                    Val::Num(n2) => Val::Num(n1 + n2),
-                    _ => panic!("Got non-number as second operand to +"),
-                },
+            match (v1, v2) {
+                (Val::Num(n1), Val::Num(n2)) => Val::Num(n1 + n2),
                 _ => panic!("Got non-number as second operand to +"),
             }
         }
         Ast::OperatorNode(Op::Times, e1, e2) => {
             let v1 = interpret_env(*e1, env.clone());
             let v2 = interpret_env(*e2, env.clone());
-            match v1 {
-                Val::Num(n1) => match v2 {
-                    Val::Num(n2) => Val::Num(n1 * n2),
-                    _ => panic!("Got non-number as second operand to +"),
-                },
-                _ => panic!("Got non-number as second operand to +"),
+            match (v1, v2) {
+                (Val::Num(n1), Val::Num(n2)) => Val::Num(n1 * n2),
+                _ => panic!("Got non-number as second operand to *"),
             }
         }
     }
