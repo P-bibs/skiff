38bba0f50416f9a714a3758abe2d5a7afb0853a8
Paul Biberstein <biberstein.paul@gmail.com>
2021-05-04 20:34:18 -0400

    Add error reporting for error tokens

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ use interpreter::interpret;
 use lexer::lex;
 use logos::Logos;
 use parser::parse;
-use std::fs;
 use std::{borrow::Borrow, error};
+use std::{fmt, fs};
 use structopt::StructOpt;
 
 /// The interpreter for the Skiff programming language
@@ -32,6 +32,15 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+#[derive(PartialEq, Debug)]
+pub struct SkiffError<'a>(pub &'a str);
+impl<'a> fmt::Display for SkiffError<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+impl<'a> error::Error for SkiffError<'a> {}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     let args = Cli::from_args();
 
@@ -40,6 +49,19 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let lexer = lex::Token::lexer(&raw);
 
     let mut token_vec: Vec<_> = lexer.spanned().collect();
+
+    for (token, span) in &token_vec {
+        if token == &lex::Token::Error {
+            error_handling::pretty_print_error(
+                "Invalid token",
+                span.clone(),
+                raw.borrow(),
+                args.path.clone(),
+            );
+            return Err(Box::new(SkiffError("Avast! Skiff execution failed")));
+        }
+    }
+
     token_vec.reverse();
 
     let parsed = parse::parse_program(&mut token_vec)?;
@@ -55,7 +77,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         Ok(output) => output,
         Err(interpret::InterpError(msg, span)) => {
             error_handling::pretty_print_error(msg.borrow(), span, raw.borrow(), args.path);
-            return Ok(());
+            return Err(Box::new(SkiffError("Avast! Skiff execution failed")));
         }
     };
 
