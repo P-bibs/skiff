5df4872aeb09367dc35a9be60ad2c5677d2999a7
Paul Biberstein <biberstein.paul@gmail.com>
2021-06-15 23:33:34 -0400

    Add macro for throwing interp errors

--- src/interpreter/interpret.rs
+++ src/interpreter/interpret.rs
@@ -5,6 +5,21 @@ use std::convert::TryInto;
 use std::{borrow::Borrow, error};
 use std::{fmt, ops::Range};
 
+macro_rules! make_throw_interp_error {
+    ($src_loc:expr, $env:expr, $stack:expr) => {
+        macro_rules! throw_interp_error {
+            ($msg:expr) => {
+                return Err(InterpError(
+                    $msg.to_string(),
+                    $src_loc.span.clone(),
+                    $env,
+                    $stack.clone(),
+                ));
+            };
+        }
+    };
+}
+
 #[derive(PartialEq, Clone, Debug)]
 pub struct StackFrame {
     src_loc: SrcLoc,
@@ -175,6 +190,7 @@ fn interpret_expr(
     func_table: &Env,
     stack: &Stack,
 ) -> Result<Val, InterpError> {
+    make_throw_interp_error!(expr.src_loc, env, stack);
     match &expr.node {
         AstNode::NumberNode(n) => Ok(Val::Num(n.clone())),
         AstNode::BoolNode(v) => Ok(Val::Bool(v.clone())),
@@ -182,12 +198,7 @@ fn interpret_expr(
             Some(v) => Ok(v.clone()),
             None => match func_table.get(id) {
                 Some(v) => Ok(v.clone()),
-                None => Err(InterpError(
-                    format!("Couldn't find var in environment: {}", id).to_string(),
-                    expr.src_loc.span.clone(),
-                    env,
-                    stack.clone(),
-                )),
+                None => throw_interp_error!(format!("Couldn't find var in environment: {}", id)),
             },
         },
         AstNode::LetNode(id, binding, body) => interpret_expr(
@@ -199,21 +210,12 @@ fn interpret_expr(
             func_table,
             stack,
         ),
-        AstNode::LetNodeTopLevel(_, _) => Err(InterpError(
-            "Found LetNodeTopLevel instead of LetNode in expression".to_string(),
-            expr.src_loc.span.clone(),
-            env,
-            stack.clone(),
-        )),
-        AstNode::BinOpNode(op, e1, e2) => interpret_binop(
-            *op,
-            e1,
-            e2,
-            expr.src_loc.span.clone(),
-            env,
-            func_table,
-            stack,
-        ),
+        AstNode::LetNodeTopLevel(_, _) => {
+            throw_interp_error!("Found LetNodeTopLevel instead of LetNode in expression".to_string())
+        }
+        AstNode::BinOpNode(op, e1, e2) => {
+            interpret_binop(*op, e1, e2, expr.src_loc.clone(), env, func_table, stack)
+        }
         AstNode::LambdaNode(params, body) => {
             Ok(Val::Lam(params.clone(), *body.clone(), env.clone()))
         }
@@ -222,16 +224,10 @@ fn interpret_expr(
             match fun_value {
                 Val::Lam(params, body, lam_env) => {
                     if params.len() != args.len() {
-                        return Err(InterpError(
-                            format!(
-                                "Function takes {} arguments but {} were provided",
-                                params.len(),
-                                args.len()
-                            )
-                            .to_string(),
-                            expr.src_loc.span.clone(),
-                            env,
-                            stack.clone(),
+                        throw_interp_error!(format!(
+                            "Function takes {} arguments but {} were provided",
+                            params.len(),
+                            args.len()
                         ));
                     }
 
@@ -252,12 +248,7 @@ fn interpret_expr(
                     // evaluate the body
                     interpret_expr(&body, lam_env, func_table, &new_stack)
                 }
-                _ => Err(InterpError(
-                    "Function call with non-function value".to_string(),
-                    expr.src_loc.span.clone(),
-                    env,
-                    stack.clone(),
-                )),
+                _ => throw_interp_error!("Function call with non-function value".to_string()),
             }
         }
         AstNode::IfNode(conditions_and_bodies, alternate) => {
@@ -265,30 +256,15 @@ fn interpret_expr(
                 match interpret_expr(condition, env.clone(), func_table, stack)? {
                     Val::Bool(true) => return interpret_expr(body, env.clone(), func_table, stack),
                     Val::Bool(false) => continue,
-                    _ => {
-                        return Err(InterpError(
-                            "Conditional expression with non-boolean condition".to_string(),
-                            expr.src_loc.span.clone(),
-                            env,
-                            stack.clone(),
-                        ))
-                    }
+                    _ => throw_interp_error!("Conditional expression with non-boolean condition"),
                 }
             }
             return interpret_expr(alternate, env.clone(), func_table, stack);
         }
-        AstNode::FunctionNode(_, _, _) => Err(InterpError(
-            "Function node not at top level".to_string(),
-            expr.src_loc.span.clone(),
-            env,
-            stack.clone(),
-        )),
-        AstNode::DataDeclarationNode(_, _) => Err(InterpError(
-            "Found DataDeclarationNode instead of LetNode in expression".to_string(),
-            expr.src_loc.span.clone(),
-            env,
-            stack.clone(),
-        )),
+        AstNode::FunctionNode(_, _, _) => throw_interp_error!("Function node not at top level"),
+        AstNode::DataDeclarationNode(_, _) => {
+            throw_interp_error!("Found DataDeclarationNode instead of LetNode in expression")
+        }
         AstNode::DataLiteralNode(discriminant, fields) => {
             let mut values = vec![];
             for expr in fields {
@@ -303,12 +279,7 @@ fn interpret_expr(
                     return interpret_expr(expr, env.union(match_env), func_table, stack);
                 }
             }
-            Err(InterpError(
-                "No branch of match expression matched value".to_string(),
-                expr.src_loc.span.clone(),
-                env,
-                stack.clone(),
-            ))
+            throw_interp_error!("No branch of match expression matched value")
         }
     }
 }
@@ -358,24 +329,24 @@ fn match_pattern_with_value(pattern: &Pattern, value: &Val) -> Option<Env> {
 }
 
 macro_rules! interpret_binop {
-    ($value1:ident, $value2:ident, $span: expr, $op:tt, $type1:ident, $type2:ident, $output_type:ident, $env:ident, $stack:ident) => {
+    ($value1:ident, $value2:ident, $src_loc: expr, $op:tt, $type1:ident, $type2:ident, $output_type:ident, $env:ident, $stack:ident) => {
         match ($value1, $value2) {
             (Val::$type1(xv), Val::$type2(yv)) => Ok(Val::$output_type(xv $op yv)),
             (Val::$type1(_), e) => Err(InterpError(
                 format!("Bad second op to {}: {}", stringify!($op), e).to_string(),
-                $span,
+                $src_loc.span,
                 $env,
                 $stack.clone(),
             )),
             (e, Val::$type2(_)) => Err(InterpError(
                 format!("Bad first op to {}: {}", stringify!($op), e).to_string(),
-                $span,
+                $src_loc.span,
                 $env,
                 $stack.clone(),
             )),
             (e1, e2) => Err(InterpError(
                 format!("Bad ops to {}: {}\n{}", stringify!($op), e1, e2).to_string(),
-                $span,
+                $src_loc.span,
                 $env,
                 $stack.clone(),
             )),
@@ -387,50 +358,37 @@ fn interpret_binop(
     op: BinOp,
     e1: &Ast,
     e2: &Ast,
-    span: Range<usize>,
+    src_loc: SrcLoc,
     env: Env,
     func_table: &Env,
     stack: &Stack,
 ) -> Result<Val, InterpError> {
+    make_throw_interp_error!(src_loc, env, stack);
+
     let v1 = interpret_expr(e1, env.clone(), func_table, stack)?;
     let v2 = interpret_expr(e2, env.clone(), func_table, stack)?;
 
     match op {
-        BinOp::Plus => interpret_binop!(v1, v2, span, +, Num, Num, Num, env, stack),
-        BinOp::Minus => interpret_binop!(v1, v2, span, -, Num, Num, Num, env, stack),
-        BinOp::Times => interpret_binop!(v1, v2, span, *, Num, Num, Num, env, stack),
-        BinOp::Divide => interpret_binop!(v1, v2, span, /, Num, Num, Num, env, stack),
-        BinOp::Modulo => interpret_binop!(v1, v2, span, %, Num, Num, Num, env, stack),
+        BinOp::Plus => interpret_binop!(v1, v2, src_loc, +, Num, Num, Num, env, stack),
+        BinOp::Minus => interpret_binop!(v1, v2, src_loc, -, Num, Num, Num, env, stack),
+        BinOp::Times => interpret_binop!(v1, v2, src_loc, *, Num, Num, Num, env, stack),
+        BinOp::Divide => interpret_binop!(v1, v2, src_loc, /, Num, Num, Num, env, stack),
+        BinOp::Modulo => interpret_binop!(v1, v2, src_loc, %, Num, Num, Num, env, stack),
         BinOp::Exp => match (v1, v2) {
             (Val::Num(xv), Val::Num(yv)) => Ok(Val::Num(xv.pow(yv.try_into().unwrap()))),
-            (Val::Num(_), e) => Err(InterpError(
-                format!("Bad second op to {}: {}", "**", e).to_string(),
-                span,
-                env,
-                stack.clone(),
-            )),
-            (e, Val::Num(_)) => Err(InterpError(
-                format!("Bad first op to {}: {}", "**", e).to_string(),
-                span,
-                env,
-                stack.clone(),
-            )),
-            (e1, e2) => Err(InterpError(
-                format!("Bad ops to {}: {}\n{}", "**", e1, e2).to_string(),
-                span,
-                env,
-                stack.clone(),
-            )),
+            (Val::Num(_), e) => throw_interp_error!(format!("Bad second op to {}: {}", "**", e)),
+            (e, Val::Num(_)) => throw_interp_error!(format!("Bad first op to {}: {}", "**", e)),
+            (e1, e2) => throw_interp_error!(format!("Bad ops to {}: {}\n{}", "**", e1, e2)),
         },
         BinOp::Eq => Ok(Val::Bool(v1 == v2)),
-        BinOp::Gt => interpret_binop!(v1, v2, span, >, Num, Num, Bool, env, stack),
-        BinOp::Lt => interpret_binop!(v1, v2, span, <, Num, Num, Bool, env, stack),
-        BinOp::GtEq => interpret_binop!(v1, v2, span, >=, Num, Num, Bool, env, stack),
-        BinOp::LtEq => interpret_binop!(v1, v2, span, <=, Num, Num, Bool, env, stack),
-        BinOp::LAnd => interpret_binop!(v1, v2, span, &&, Bool, Bool, Bool, env, stack),
-        BinOp::LOr => interpret_binop!(v1, v2, span, ||, Bool, Bool, Bool, env, stack),
-        BinOp::BitAnd => interpret_binop!(v1, v2, span, &, Num, Num, Num, env, stack),
-        BinOp::BitOr => interpret_binop!(v1, v2, span, |, Num, Num, Num, env, stack),
-        BinOp::BitXor => interpret_binop!(v1, v2, span, ^, Num, Num, Num, env, stack),
+        BinOp::Gt => interpret_binop!(v1, v2, src_loc, >, Num, Num, Bool, env, stack),
+        BinOp::Lt => interpret_binop!(v1, v2, src_loc, <, Num, Num, Bool, env, stack),
+        BinOp::GtEq => interpret_binop!(v1, v2, src_loc, >=, Num, Num, Bool, env, stack),
+        BinOp::LtEq => interpret_binop!(v1, v2, src_loc, <=, Num, Num, Bool, env, stack),
+        BinOp::LAnd => interpret_binop!(v1, v2, src_loc, &&, Bool, Bool, Bool, env, stack),
+        BinOp::LOr => interpret_binop!(v1, v2, src_loc, ||, Bool, Bool, Bool, env, stack),
+        BinOp::BitAnd => interpret_binop!(v1, v2, src_loc, &, Num, Num, Num, env, stack),
+        BinOp::BitOr => interpret_binop!(v1, v2, src_loc, |, Num, Num, Num, env, stack),
+        BinOp::BitXor => interpret_binop!(v1, v2, src_loc, ^, Num, Num, Num, env, stack),
     }
 }
