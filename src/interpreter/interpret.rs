ab7db484ab617db4f66f00b1b5ea747fbadfaa73
Paul Biberstein <biberstein.paul@gmail.com>
2021-07-17 21:29:41 -0400

    Wrap interpret context in struct

--- src/interpreter/interpret.rs
+++ src/interpreter/interpret.rs
@@ -12,7 +12,7 @@ macro_rules! make_throw_interp_error {
                 return Err(InterpError(
                     $msg.to_string(),
                     $src_loc.span.clone(),
-                    $env,
+                    $env.clone(),
                     $stack.clone(),
                 ));
             };
@@ -69,6 +69,29 @@ impl fmt::Display for InterpError {
     }
 }
 impl error::Error for InterpError {}
+
+#[derive(PartialEq, Debug, Clone, Copy, Hash)]
+pub struct InterpretContext<'a> {
+    pub env: &'a Env,
+    pub func_table: &'a Env,
+    pub stack: &'a Stack,
+}
+impl<'a> InterpretContext<'a> {
+    pub fn new(env: &'a Env, func_table: &'a Env, stack: &'a Stack) -> Self {
+        InterpretContext {
+            env,
+            func_table,
+            stack,
+        }
+    }
+    pub fn new_env(&self, env: &'a Env) -> Self {
+        InterpretContext {
+            env,
+            func_table: self.func_table,
+            stack: self.stack,
+        }
+    }
+}
 
 pub fn interpret(program: &Program) -> Result<Vec<Val>, InterpError> {
     let data_funcs_ast = find_data_declarations(program)?;
@@ -170,9 +193,7 @@ fn interpret_top_level(expr: &Ast, env: Env, func_table: &Env) -> Result<ValOrEn
         AstNode::LetNodeTopLevel(id, binding) => {
             let val = interpret_expr(
                 binding.borrow(),
-                env.clone(),
-                func_table,
-                &StackFrame::new_stack(),
+                InterpretContext::new(&env, func_table, &StackFrame::new_stack()),
             )?;
             Ok(ValOrEnv::E(env.update(id.id.clone(), val)))
         }
@@ -186,20 +207,20 @@ fn interpret_top_level(expr: &Ast, env: Env, func_table: &Env) -> Result<ValOrEn
         AstNode::DataDeclarationNode(_, _) => Ok(ValOrEnv::E(env)),
         _ => Ok(ValOrEnv::V(interpret_expr(
             expr,
-            env,
-            func_table,
-            &StackFrame::new_stack(),
+            InterpretContext::new(&env, func_table, &StackFrame::new_stack()),
         )?)),
     }
 }
 
-fn interpret_expr(
-    expr: &Ast,
-    env: Env,
-    func_table: &Env,
-    stack: &Stack,
-) -> Result<Val, InterpError> {
+fn interpret_expr(expr: &Ast, context: InterpretContext) -> Result<Val, InterpError> {
+    let InterpretContext {
+        env,
+        func_table,
+        stack,
+    } = context.clone();
+
     make_throw_interp_error!(expr.src_loc, env, stack);
+
     match &expr.node {
         AstNode::NumberNode(n) => Ok(Val::Num(n.clone())),
         AstNode::BoolNode(v) => Ok(Val::Bool(v.clone())),
@@ -212,18 +233,13 @@ fn interpret_expr(
         },
         AstNode::LetNode(id, binding, body) => interpret_expr(
             body,
-            env.update(
-                id.id.clone(),
-                interpret_expr(binding, env.clone(), func_table, stack)?,
-            ),
-            func_table,
-            stack,
+            context.new_env(&env.update(id.id.clone(), interpret_expr(binding, context)?)),
         ),
         AstNode::LetNodeTopLevel(_, _) => {
             throw_interp_error!("Found LetNodeTopLevel instead of LetNode in expression".to_string())
         }
         AstNode::BinOpNode(op, e1, e2) => {
-            interpret_binop(*op, e1, e2, expr.src_loc.clone(), env, func_table, stack)
+            interpret_binop(*op, e1, e2, expr.src_loc.clone(), context)
         }
         AstNode::LambdaNode(params, body) => Ok(Val::Lam(
             params.iter().map(|id| id.id.clone()).collect(),
@@ -231,7 +247,7 @@ fn interpret_expr(
             env.clone(),
         )),
         AstNode::FunCallNode(fun, args) => {
-            let fun_value = interpret_expr(fun, env.clone(), func_table, stack)?;
+            let fun_value = interpret_expr(fun, context)?;
             match fun_value {
                 Val::Lam(params, body, lam_env) => {
                     if params.len() != args.len() {
@@ -244,10 +260,7 @@ fn interpret_expr(
 
                     let mut new_env: Env = HashMap::new();
                     for (param, arg) in params.iter().zip(args) {
-                        new_env.insert(
-                            param.to_string(),
-                            interpret_expr(arg, env.clone(), func_table, stack)?,
-                        );
+                        new_env.insert(param.to_string(), interpret_expr(arg, context)?);
                     }
                     // Make the new stack and frame
                     let new_frame = StackFrame::new(expr.src_loc.clone(), new_env.clone());
@@ -257,20 +270,23 @@ fn interpret_expr(
                     let mut lam_env = lam_env.clone();
                     lam_env.extend(new_env);
                     // evaluate the body
-                    interpret_expr(&body, lam_env, func_table, &new_stack)
+                    interpret_expr(
+                        &body,
+                        InterpretContext::new(&lam_env, func_table, &new_stack),
+                    )
                 }
                 _ => throw_interp_error!("Function call with non-function value".to_string()),
             }
         }
         AstNode::IfNode(conditions_and_bodies, alternate) => {
             for (condition, body) in conditions_and_bodies {
-                match interpret_expr(condition, env.clone(), func_table, stack)? {
-                    Val::Bool(true) => return interpret_expr(body, env.clone(), func_table, stack),
+                match interpret_expr(condition, context)? {
+                    Val::Bool(true) => return interpret_expr(body, context),
                     Val::Bool(false) => continue,
                     _ => throw_interp_error!("Conditional expression with non-boolean condition"),
                 }
             }
-            return interpret_expr(alternate, env.clone(), func_table, stack);
+            return interpret_expr(alternate, context);
         }
         AstNode::FunctionNode(_, _, _, _) => throw_interp_error!("Function node not at top level"),
         AstNode::DataDeclarationNode(_, _) => {
@@ -279,15 +295,15 @@ fn interpret_expr(
         AstNode::DataLiteralNode(discriminant, fields) => {
             let mut values = vec![];
             for expr in fields {
-                values.push(interpret_expr(expr, env.clone(), func_table, stack)?);
+                values.push(interpret_expr(expr, context)?);
             }
             return Ok(Val::Data(discriminant.clone(), values));
         }
         AstNode::MatchNode(expression_to_match, branches) => {
             for (pattern, expr) in branches {
-                let val = interpret_expr(expression_to_match, env.clone(), func_table, stack)?;
+                let val = interpret_expr(expression_to_match, context)?;
                 if let Some(match_env) = match_pattern_with_value(pattern, &val) {
-                    return interpret_expr(expr, env.union(match_env), func_table, stack);
+                    return interpret_expr(expr, context.new_env(&env.clone().union(match_env)));
                 }
             }
             throw_interp_error!("No branch of match expression matched value")
@@ -346,19 +362,19 @@ macro_rules! interpret_binop {
             (Val::$type1(_), e) => Err(InterpError(
                 format!("Bad second op to {}: {}", stringify!($op), e).to_string(),
                 $src_loc.span,
-                $env,
+                $env.clone(),
                 $stack.clone(),
             )),
             (e, Val::$type2(_)) => Err(InterpError(
                 format!("Bad first op to {}: {}", stringify!($op), e).to_string(),
                 $src_loc.span,
-                $env,
+                $env.clone(),
                 $stack.clone(),
             )),
             (e1, e2) => Err(InterpError(
                 format!("Bad ops to {}: {}\n{}", stringify!($op), e1, e2).to_string(),
                 $src_loc.span,
-                $env,
+                $env.clone(),
                 $stack.clone(),
             )),
         }
@@ -370,14 +386,14 @@ fn interpret_binop(
     e1: &Ast,
     e2: &Ast,
     src_loc: SrcLoc,
-    env: Env,
-    func_table: &Env,
-    stack: &Stack,
+    context: InterpretContext,
 ) -> Result<Val, InterpError> {
+    let InterpretContext { env, stack, .. } = context.clone();
+
     make_throw_interp_error!(src_loc, env, stack);
 
-    let v1 = interpret_expr(e1, env.clone(), func_table, stack)?;
-    let v2 = interpret_expr(e2, env.clone(), func_table, stack)?;
+    let v1 = interpret_expr(e1, context)?;
+    let v2 = interpret_expr(e2, context)?;
 
     match op {
         BinOp::Plus => interpret_binop!(v1, v2, src_loc, +, Num, Num, Num, env, stack),
