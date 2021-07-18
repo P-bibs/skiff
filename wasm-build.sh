35424d59d04f247dfc6406cb9918f5e768965de0
Paul Biberstein <biberstein.paul@gmail.com>
2021-07-17 22:57:40 -0400

    Update wasm build script

--- wasm-build.sh
+++ wasm-build.sh
@@ -1,8 +1,9 @@
 mv Cargo.toml Cargo.desktop.toml
 mv Cargo.wasm.toml Cargo.toml
-~/.cargo/bin/wasm-pack build
+wasm-pack build
 mv Cargo.toml Cargo.wasm.toml
 mv Cargo.desktop.toml Cargo.toml
 echo ""
 echo "Package built"
 echo "Remember to update name, version, and add snippets directory"
+echo "`wasm-pack publish` to publish"
