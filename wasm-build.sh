bdc77295dc9970e5367eecc942f41e080ec7e7f5
Paul Biberstein <biberstein.paul@gmail.com>
2021-08-12 21:01:39 -0400

    Update wasm build script

--- wasm-build.sh
+++ wasm-build.sh
@@ -1,9 +1,14 @@
 mv Cargo.toml Cargo.desktop.toml
 mv Cargo.wasm.toml Cargo.toml
+
 wasm-pack build
+
+rm -r skiff-lang
+mv pkg skiff-lang
+
 mv Cargo.toml Cargo.wasm.toml
 mv Cargo.desktop.toml Cargo.toml
 echo ""
 echo "Package built"
-echo "Remember to update name, version, and add snippets directory"
+echo "Remember to add snippets directory"
 echo "'wasm-pack publish' to publish"
